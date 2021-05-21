ac355a3ed7354e1d3d1d492d86b19db7b237e6f9
Sean Bryan <sb5416@hotmail.com>
2021-05-21 21:32:18 +1000

    cleared old code skeleton

--- cpp/init.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// function declarations:
-void init_noise(float * phi1, float * phi2, float * phi1dot, float *phi2dot);
-void gaussian_thermal(float * phi1, float * phi2, float * phi1dot, float *phi2dot, bool flag_normalise);
--- cpp/spatial.cpp
+++
@@ -1,6 +0,0 @@
-
-#include "spatial.h"
-
-// function implementations:
-
-// TODO:
