d10df0ee1cf9c4583d93e77c5cb1df4d6a311515
Sean Bryan <sb5416@hotmail.com>
2021-10-04 17:46:06 +1100

    fixed 2D xi normalisation

--- src/standard/run.cpp
+++ src/standard/run.cpp
@@ -72,6 +72,7 @@ void run_standard() {
 
     if (parameters.sample_time_series) {
         fprintf(fp_time_series, "time,");
+        fprintf(fp_time_series, "scalefactor,");
         if (parameters.sample_strings) fprintf(fp_time_series, "xi,");
         if (parameters.sample_background) fprintf(fp_time_series, "phi1_bar,phi2_bar,phidot1_bar,phidot2_bar,axion_bar,saxion_bar");
         fprintf(fp_time_series, "\n");
@@ -85,6 +86,7 @@ void run_standard() {
         if (should_sample_time_series(tstep, parameters.n_samples, final_step)) {
 
             fprintf(fp_time_series, "%f", physical_time());
+            fprintf(fp_time_series, ",%f", scale_factor());
 
             if (parameters.sample_strings) {
 
@@ -97,8 +99,6 @@ void run_standard() {
                     std::vector <vec2i> s;
                     int n_plaquettes = cores2(data.axion, s);
 
-                    // TODO: replace with statistical estimate?
-
                     // Count neighbouring plaquettes:
                     int count = 0;
                     for (int i = 0; i < n_plaquettes - 1; i++) {
@@ -107,10 +107,12 @@ void run_standard() {
                         if (diff_y == 0 && diff_x == 1) count += 1;
                         if (diff_y == 1 && diff_x == 0) count += 1;
                     }
+                    int num_cores = n_plaquettes - count;
 
                     float t_phys = physical_time();
-                    int num_cores = n_plaquettes - count;
-                    float xi = num_cores * pow_2(t_phys / tau);
+                    float a = scale_factor();
+                    float physical_area = pow_2(parameters.N * parameters.space_step * a);
+                    float xi = num_cores * pow_2(t_phys) / physical_area;
                     fprintf(fp_time_series, ",%f", xi);
 
                 } else {
@@ -132,7 +134,8 @@ void run_standard() {
             if (parameters.sample_background) {
                 dtype phi1_bar, phi2_bar, phidot1_bar, phidot2_bar, axion_bar, saxion_bar;
                 phi1_bar = phi2_bar = phidot1_bar = phidot2_bar = axion_bar = saxion_bar = 0.0f;
-                #pragma omp parallel for schedule(static) reduction(+:phi1_bar,phi2_bar,phidot1_bar,phidot2_bar)
+
+                #pragma omp parallel for schedule(static) reduction(+:phi1_bar,phi2_bar,phidot1_bar,phidot2_bar,axion_bar,saxion_bar)
                 for (int i = 0; i < length; i++) {
                     phi1_bar += data.phi1[i];
                     phi2_bar += data.phi2[i];
@@ -141,6 +144,7 @@ void run_standard() {
                     axion_bar += atan2(data.phi1[i], data.phi2[i]);
                     saxion_bar += sqrt(pow_2(data.phi1[i]) + pow_2(data.phi2[i]));
                 }
+
                 fprintf(fp_time_series, ",%f,%f,%f,%f,%f,%f", phi1_bar / length, phi2_bar / length, phidot1_bar / length, phidot2_bar / length, axion_bar / length, saxion_bar / length);
             }
 
