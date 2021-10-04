6521d8a419013dd45bd0b41168be304eb04d3724
Sean Bryan <sb5416@hotmail.com>
2021-10-04 17:47:23 +1100

    moved space integration with time integration

--- src/standard/integrate.cpp
+++ src/standard/integrate.cpp
@@ -2,6 +2,7 @@
 
 void build_coefficient_matrix(sparse_matrix_t *handle, int NDIMS, int N) {
 
+    // stencil coefficients: https://en.wikipedia.org/wiki/Finite_difference_coefficient
     int stencil_setting = parameters.stencil_setting;
     std::vector<std::vector<dtype>> coefficients = {
         {        -2.0f * NDIMS,      1.0f },                                       // stencil_setting = 0
@@ -175,33 +176,6 @@ dtype laplacian3D(dtype *phi, int i, int j, int k, float dx, int N) {
 }
 
 /*
- * Compute the kernel given the fields. The kernel follows from the field
- * rescaled equation of motion:
- *    K[\phi] = \nabla^2 \phi - \lambda_{PRS} \tau^{-2} \phi (|\phi|^2 - \tau^2 + T_0^2/3)
- */
-static void kernels_field_rescaled(dtype *ker1, dtype *ker2, all_data data) {
-
-    int length = get_length();
-    float dx = parameters.space_step;
-
-    mkl_wrapper_sparse_mv(SPARSE_OPERATION_NON_TRANSPOSE, 1.0f / dx, data.coefficient_matrix, (matrix_descr) { SPARSE_MATRIX_TYPE_SYMMETRIC, SPARSE_FILL_MODE_UPPER, SPARSE_DIAG_NON_UNIT }, data.phi1, 0.0f, ker1);
-
-    mkl_wrapper_sparse_mv(SPARSE_OPERATION_NON_TRANSPOSE, 1.0f / dx, data.coefficient_matrix, (matrix_descr) { SPARSE_MATRIX_TYPE_SYMMETRIC, SPARSE_FILL_MODE_UPPER, SPARSE_DIAG_NON_UNIT }, data.phi2, 0.0f, ker2);
-
-    #pragma omp parallel for schedule(static)
-    for (int i = 0; i < length; i++) {
-        ker1[i] += - 1.0f / pow_2(tau) * parameters.lambdaPRS * data.phi1[i] * (
-                     pow_2(data.phi1[i]) + pow_2(data.phi2[i]) - pow_2(tau)
-                   + pow_2(T_initial) / (3.0f)
-                   );
-        ker2[i] += - 1.0f / pow_2(tau) * parameters.lambdaPRS * data.phi2[i] * (
-                     pow_2(data.phi1[i]) + pow_2(data.phi2[i]) - pow_2(tau)
-                   + pow_2(T_initial) / (3.0f)
-                   );
-    }
-}
-
-/*
  * Velocity-Verlet (synchronised leapfrog) time evolution algorithm, see equation (125)
  * in arXiv:2006.15122v2 'The art of simulating the early
  * Universe'.
@@ -212,6 +186,7 @@ void vvsl_field_rescaled(all_data data) {
 
     int length = get_length();
     float dt = parameters.time_step;
+    float dx = parameters.space_step;
 
     #pragma omp parallel for schedule(static)
     for (int i = 0; i < length; i++) {
@@ -221,7 +196,17 @@ void vvsl_field_rescaled(all_data data) {
 
     tau = tau + dt;
 
-    kernels_field_rescaled(data.ker1_next, data.ker2_next, data);
+    // compute kernels:
+
+    mkl_wrapper_sparse_mv(SPARSE_OPERATION_NON_TRANSPOSE, 1.0f / dx, data.coefficient_matrix, (matrix_descr) { SPARSE_MATRIX_TYPE_SYMMETRIC, SPARSE_FILL_MODE_UPPER, SPARSE_DIAG_NON_UNIT }, data.phi1, 0.0f, data.ker1_next);
+
+    mkl_wrapper_sparse_mv(SPARSE_OPERATION_NON_TRANSPOSE, 1.0f / dx, data.coefficient_matrix, (matrix_descr) { SPARSE_MATRIX_TYPE_SYMMETRIC, SPARSE_FILL_MODE_UPPER, SPARSE_DIAG_NON_UNIT }, data.phi2, 0.0f, data.ker2_next);
+
+    #pragma omp parallel for schedule(static)
+    for (int i = 0; i < length; i++) {
+        data.ker1_next[i] += - 1.0f / pow_2(tau) * parameters.lambdaPRS * data.phi1[i] * (pow_2(data.phi1[i]) + pow_2(data.phi2[i]) - pow_2(tau) + pow_2(T_initial) / (3.0f));
+        data.ker2_next[i] += - 1.0f / pow_2(tau) * parameters.lambdaPRS * data.phi2[i] * (pow_2(data.phi1[i]) + pow_2(data.phi2[i]) - pow_2(tau) + pow_2(T_initial) / (3.0f));
+    }
 
     #pragma omp parallel for schedule(static)
     for (int i = 0; i < length; i++) {
@@ -233,35 +218,6 @@ void vvsl_field_rescaled(all_data data) {
     for (int i = 0; i < length; i++) {
         data.ker1_curr[i] = data.ker1_next[i];
         data.ker2_curr[i] = data.ker2_next[i];
-    }
-}
-
-
-/*
- * Compute the kernel given the fields. The kernel follows from the Hamiltonian-like
- * equation of motion (see Eq. (219-221) in arXiv:2006.15122v2):
- *    K[\phi] = \tau^2 \nabla^2 \phi - \lambda_{PRS} \tau^{2} \phi (|\phi|^2 - 1 + T_0^2/(3 \tau^2))
- * Note: in this case, phidot is really pi where pi = phidot * a^2.
- */
-static void kernels_hamiltonian_form(dtype *ker1, dtype *ker2, all_data data) {
-
-    int length = get_length();
-    float dx = parameters.space_step;
-
-    mkl_wrapper_sparse_mv(SPARSE_OPERATION_NON_TRANSPOSE, pow_2(tau) / dx, data.coefficient_matrix, (matrix_descr) { SPARSE_MATRIX_TYPE_SYMMETRIC, SPARSE_FILL_MODE_UPPER, SPARSE_DIAG_NON_UNIT }, data.phi1, 0.0f, ker1);
-
-    mkl_wrapper_sparse_mv(SPARSE_OPERATION_NON_TRANSPOSE, pow_2(tau) / dx, data.coefficient_matrix, (matrix_descr) { SPARSE_MATRIX_TYPE_SYMMETRIC, SPARSE_FILL_MODE_UPPER, SPARSE_DIAG_NON_UNIT }, data.phi2, 0.0f, ker2);
-
-    #pragma omp parallel for schedule(static)
-    for (int i = 0; i < length; i++) {
-        ker1[i] += - pow_2(tau) * parameters.lambdaPRS * data.phi1[i] * (
-                     pow_2(data.phi1[i]) + pow_2(data.phi2[i]) - 1.0f
-                   + pow_2(T_initial) / (3.0f * pow_2(tau))
-                   );
-        ker2[i] += - pow_2(tau) * parameters.lambdaPRS * data.phi2[i] * (
-                     pow_2(data.phi1[i]) + pow_2(data.phi2[i]) - 1.0f
-                   + pow_2(T_initial) / (3.0f * pow_2(tau))
-                   );
     }
 }
 
@@ -277,6 +233,7 @@ void vvsl_hamiltonian_form(all_data data) {
 
     int length = get_length();
     float dt = parameters.time_step;
+    float dx = parameters.space_step;
 
     tau = tau + dt;
 
@@ -286,7 +243,17 @@ void vvsl_hamiltonian_form(all_data data) {
         data.phi2[i] += dt / pow_2(tau) * (data.phidot2[i] + 0.5f * dt * data.ker2_curr[i]);
     }
 
-    kernels_hamiltonian_form(data.ker1_next, data.ker2_next, data);
+    // compute kernels:
+
+    mkl_wrapper_sparse_mv(SPARSE_OPERATION_NON_TRANSPOSE, pow_2(tau) / dx, data.coefficient_matrix, (matrix_descr) { SPARSE_MATRIX_TYPE_SYMMETRIC, SPARSE_FILL_MODE_UPPER, SPARSE_DIAG_NON_UNIT }, data.phi1, 0.0f, data.ker1_next);
+
+    mkl_wrapper_sparse_mv(SPARSE_OPERATION_NON_TRANSPOSE, pow_2(tau) / dx, data.coefficient_matrix, (matrix_descr) { SPARSE_MATRIX_TYPE_SYMMETRIC, SPARSE_FILL_MODE_UPPER, SPARSE_DIAG_NON_UNIT }, data.phi2, 0.0f, data.ker2_next);
+
+    #pragma omp parallel for schedule(static)
+    for (int i = 0; i < length; i++) {
+        data.ker1_next[i] += - pow_2(tau) * parameters.lambdaPRS * data.phi1[i] * (pow_2(data.phi1[i]) + pow_2(data.phi2[i]) - 1.0f + pow_2(T_initial) / (3.0f * pow_2(tau)));
+        data.ker2_next[i] += - pow_2(tau) * parameters.lambdaPRS * data.phi2[i] * (pow_2(data.phi1[i]) + pow_2(data.phi2[i]) - 1.0f + pow_2(T_initial) / (3.0f * pow_2(tau)));
+    }
 
     #pragma omp parallel for schedule(static)
     for (int i = 0; i < length; i++) {
