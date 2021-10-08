188fa5ae03fcddab792768c597834577ee2c08c8
Sean Bryan <sb5416@hotmail.com>
2021-10-08 23:54:23 +1100

    amr initial skeleton

--- src/amr/amr_internal.hpp
+++ src/amr/amr_internal.hpp
@@ -1,64 +1,31 @@
 #pragma once
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-#include <math.h>
+#include "common/common.hpp"
 
-#include <omp.h>
+/*
+ * Solution vectors contain data for all blocks/patches defined on the level.
+ * The location of the solution vector for each patch in the array is specified by
+ * the b_index array.
+ */
+typedef struct level_data {
+    // assume grids are square for simplicity
+    int size;      // size of current solution vector
 
-#include "mkl.h"
-#include "mkl_vsl.h"
-#include "mkl_spblas.h"
+    data_t *phi1;       // phi_1 field values
+    data_t *phi2;       // phi_2 field values
+    data_t *phidot1;    // phi_1 time derivative
+    data_t *phidot2;    // phi_2 time derivative
+    data_t *ker1_curr;  // current kernel for phi_1 equation of motion
+    data_t *ker2_curr;  // current kernel for phi_2 equation of motion
+    data_t *ker1_next;  // next kernel for phi_1 equation of motion
+    data_t *ker2_next;  // next kernel for phi_2 equation of motion
+    data_t *axion;      // axion field values
+    data_t *saxion;     // saxion field values
 
-#include "../parameters.h"
+    std::vector<int> b_index; // "block index": b_index[n] gives the starting index in the solution vector for the nth block.
+    std::vector<int> b_size;  // "block size": size of square grid.
 
-#ifdef USE_DOUBLE_PRECISION
-typedef double data_t;
-#else
-typedef float data_t;
-#endif
-
-typedef unsigned int uint32;
-
-typedef struct _level_data {
-    data_t *phi1; // phi_1 field values
-    data_t *phi2; // phi_2 field values
-    data_t *phidot1; // phi_1 time derivative
-    data_t *phidot2; // phi_2 time derivative
-    data_t *ker1_curr; // current kernel for phi_1 equation of motion
-    data_t *ker2_curr; // current kernel for phi_2 equation of motion
-    data_t *ker1_next; // next kernel for phi_1 equation of motion
-    data_t *ker2_next; // next kernel for phi_2 equation of motion
-
-    data_t *axion; // axion field values
-    data_t *saxion; // saxion field values
-
-
-
-    sparse_matrix_t coefficient_matrix;
+    // worry about coefficient matrix later:
+    // sparse_matrix_t coefficient_matrix;
 
 } level_data;
-
-struct Hierarchy {
-    int num_levels;
-    Level *levels; // (Array of pointers to structs)
-};
-
-struct Level {
-    int num_blocks;
-    float dx, dtau;
-    Block *blocks; // (Array of pointers to structs)
-};
-
-struct Block {
-    // For 2D case: phi1(x,y) = phi1[x + y * width]
-    int width, height;
-    float xmin, xmax;
-    float ymin, ymax;
-    float *phi1;
-    float *phi2;
-    float *phidot1;
-    float *phidot2;
-};
--- src/amr/amr_run.cpp
+++ src/amr/amr_run.cpp
@@ -1,75 +1,173 @@
-#include "amr.h"
-#include "interface.h"
 
-#define TOTAL_TIME_STEPS 100
+/*
+def initialise_coarse_grid(phi1, phi2, phidot1, phidot2, dx_coarsest):
+    '''
+    Input is a high resolution snapshot of the standard non-amr simulation. The function
+    should return a grid hierarchy with the coarsest level having a resolution defined by
+    dx_coarsest and the finest level having a resolution equal to the given snapshot.
+    We basically coarsen areas of the high resolution which are not needed in the given
+    snapshot whilst keeping the high resolution around areas of interest (strings) which
+    can be futher refined. This should hopefully decrease the number of total gridpoints
+    and speed up the simulation.
 
-void evolve_mesh_level(Hierarchy grid_hierarchy, int level);
-void regrid(Hierarchy grid_hierarchy);
-void integrate_level(Hierarchy grid_hierarchy, int level);
-void reflux(Hierarchy grid_hierarchy, int level);
-void average_down(Hierarchy grid_hierarchy, int level);
+    @phi1, @phi2, @phidot1, @phidot2: field values on a regular mesh.
+    @dx_coarsest: must be a factor of 2 multiple of dx (of the high resolution snapshot) i.e.
+    dx_coarsest = 2^k * dx for some integer k.
 
-void run_amr(float * phi1, float * phidot1, float * phi2, float * phidot2) {
+      (coarsest) level 0: grid separation = dx_coarsest / 2^0
+                 level 1: grid separation = dx_coarsest / 2^1
+                 level 2: grid separation = dx_coarsest / 2^2
+                  ...
+      (finest)   level k: grid separation = dx_coarsest / 2^k = dx
+                    where k = log2(dx_coarsest / dx)
+    '''
 
-    // Construct initial grid hierarchy:
-    Hierarchy grid_hierarchy = construct_grid_hierarchy(phi1, phidot1, phi2, phidot2);
-
-    float dtau = grid_hierarchy.levels[0].dtau;
-    
-    // TODO: ask Giovanni what he does here:
-    float t_evol = 1.0f; // dtau/DeltaRatio - dtau
-    for (int count = 0; count < TOTAL_TIME_STEPS; count++) {
-
-        evolve_mesh_level(grid_hierarchy, 0);
-
-        t_evol = t_evol + dtau;
+    grid_hierarchy = {
+        'levels': [],
     }
 
-    // save_snapshot(grid_hierarchy);
-}
+    coarse_fine_ratio = dx_coarsest // dx
+    max_levels = int(np.log2(coarse_fine_ratio) + 1)
+
+    # First define the coarsest level:
+    grid_hierarchy['levels'].append({
+        'level_id': 0,
+        'blocks': [{
+            'block_id': 0,
+            'x_range': (0, L),
+            'y_range': (0, L),
+            'phi1':    np.copy(phi1[::coarse_fine_ratio,::coarse_fine_ratio]),
+            'phi2':    np.copy(phi2[::coarse_fine_ratio,::coarse_fine_ratio]),
+            'phidot1': np.copy(phidot1[::coarse_fine_ratio,::coarse_fine_ratio]),
+            'phidot2': np.copy(phidot2[::coarse_fine_ratio,::coarse_fine_ratio]),
+        }]
+    })
+
+    add_stencil_buffer(grid_hierarchy, 0, 0)
+
+    # Note for any level k phi1[::coarse_fine_ratio // 2**k, ::coarse_fine_ratio // 2**k]
+
+    # Traverse fields over coarsest spatial step and see if 
+    # we need to refine.
+    for level in range(max_levels):
+
+        # Traverse over fields every (coarse_fine_ratio / 2**k) grid point calculating
+        # the gradients:
+        for block in grid_hierarchy['levels'][level]:
+            width, height = block['phi1'].shape
+            gradient = np.zeros(shape=(width, height))
+            # if StencilOrder == 2:
+            #     for i in range(width):
+            #         for j in range(height):
+            #             dphi[i,j] = ((-phi[np.mod(i+2,N),j]+8*phi[np.mod(i+1,N),j]-8*phi[i-1,j] + phi[i-2,j])\
+            #                 + (-phi[i,np.mod(j+2,N)] + 8*phi[i,np.mod(j+1,N)] -8*phi[i,j-1] + phi[i,j-2]))/(12*dx) 
+
+        # If we require refinement, move onto the next level 'level 1'
+        # and repeat.
+
+        # Continue until we no longer need to refine, or if we reach 
+        # the finest level.
+
+
+def add_stencil_buffer(grid_hierarchy, level_id, block_id):
+    # Stencil order is hard coded to 2.
+    pass
+*/
+
+#include "amr_internal.hpp"
+#include "amr_interface.hpp"
+
+void regrid(std::vector<level_data> hierarchy);
+void integrate_level(std::vector<level_data> hierarchy, int level);
+void reflux();
+void average_down();
 
 /*
  * Implementation of the Berger-Collela time-stepping algorithm:
  * Function takes in a grid hierarchy and evolves the level
- * specified by @level by one time step.
+ * specified by level by one time step.
  */
-void evolve_mesh_level(Hierarchy grid_hierarchy, int level) {
+void evolve_level(std::vector<level_data> hierarchy, int level) {
 
-    // TODO: when to refine grid hierarchy
-    bool should_refine_grid = false;
-    if (should_refine_grid) {
+    integrate_level(hierarchy, level); // integrate current level without knowing the fine level data
+
+    if (1) {
+        // Should refine grid:
+
         // Called at a specific timestep (not everytime as it is computationally expensive?).
-        // Buffers allow features to move within a patch between refinement periods.
-        regrid(grid_hierarchy);
+        // Solution: integrate_level() should check the refinement condition after integrating fields.
+        
+        // Buffers allow features to move within a patch between refinement periods. Can also gamble by using
+        // a very accurate refinement criterion so we can be sure we don't lose resolution of features in between 
+        // refinement periods?
+        // Solution: for now just check after every integration.
+        regrid(hierarchy);
     }
 
-    integrate_level(grid_hierarchy, level); // integrate current level without knowing the fine level data
-
-    if (level < grid_hierarchy.num_levels - 1) {
+    if (level + 1 < hierarchy.size()) {
         // There exists a finer level:
 
         // Perform subcycling:
         // For a factor of 2 refinement between levels, we need to evolve the level twice:
-        evolve_mesh_level(grid_hierarchy, level + 1);
-        evolve_mesh_level(grid_hierarchy, level + 1);
+        evolve_level(hierarchy, level + 1);
+        evolve_level(hierarchy, level + 1);
 
-        reflux(grid_hierarchy, level);
-        average_down(grid_hierarchy, level); // set covered coarse cells to be the average of fine
+        reflux();
+        average_down(); // set covered coarse cells to be the average of fine
     }
 
 }
 
-void regrid(Hierarchy grid_hierarchy) {
+void run_amr() {
 
-}
+    std::vector<level_data> hierarchy;
+    level_data root_level;
+    root_level.size = get_length();
+    root_level.b_index = {0}; // the root grid starts at index 0
+    root_level.b_size = {parameters.N};
 
-void integrate_level(Hierarchy grid_hierarchy, int level) {
+    set_physics_variables();
 
-}
+    // Sanity check on input parameters:
+    assert(parameters.N % 2 == 0); // Number of grid points should always be some power of 2.
+    assert(parameters.NDIMS == 2 || parameters.NDIMS == 3);
 
-void reflux(Hierarchy grid_hierarchy, int level) {
+    root_level.phi1      = (data_t *) calloc(root_level.size, sizeof(data_t));
+    root_level.phi2      = (data_t *) calloc(root_level.size, sizeof(data_t));
+    root_level.phidot1   = (data_t *) calloc(root_level.size, sizeof(data_t));
+    root_level.phidot2   = (data_t *) calloc(root_level.size, sizeof(data_t));
+    root_level.ker1_curr = (data_t *) calloc(root_level.size, sizeof(data_t));
+    root_level.ker2_curr = (data_t *) calloc(root_level.size, sizeof(data_t));
+    root_level.ker1_next = (data_t *) calloc(root_level.size, sizeof(data_t));
+    root_level.ker2_next = (data_t *) calloc(root_level.size, sizeof(data_t));
+    root_level.axion = NULL;
+    root_level.saxion = NULL;
 
-}
-void average_down(Hierarchy grid_hierarchy, int level) {
+    assert(root_level.phi1 != NULL && root_level.phi2 != NULL && root_level.phidot1 != NULL && root_level.phidot2 != NULL);
+    assert(root_level.ker1_curr != NULL && root_level.ker2_curr != NULL && root_level.ker1_next != NULL && root_level.ker2_next != NULL);
 
+    // Set initial field values:
+    gaussian_thermal(root_level.phi1, root_level.phi2, root_level.phidot1, root_level.phidot2);
+
+    hierarchy = {root_level};
+
+    int final_step = round(light_crossing_time / parameters.time_step) - round(parameters.space_step / parameters.time_step) + 1;
+    for (int tstep = 0; tstep < final_step; tstep++) {
+        
+        evolve_level(hierarchy, 0);
+    }
+
+    // Clean up memory:
+    for (level_data level : hierarchy) {
+        free(level.phi1);
+        free(level.phi2);
+        free(level.phidot1);
+        free(level.phidot2);
+        free(level.ker1_curr);
+        free(level.ker2_curr);
+        free(level.ker1_next);
+        free(level.ker2_next);
+        free(level.axion);
+        free(level.saxion);
+    }
 }
