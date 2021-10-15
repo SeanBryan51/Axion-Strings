e699e27305d87b5a1683cb5f5b7b535b8fc0dee6
Sean Bryan <sb5416@hotmail.com>
2021-10-15 14:22:25 +1100

    double free debug

--- src/common/pk_estimation.cpp
+++ src/common/pk_estimation.cpp
@@ -37,11 +37,12 @@ static void shift3D(fftw_complex *arr, int N) {
 void output_powerspec(char *file_name, data_t *data_real, data_t *data_imag) {
 
     int N = parameters.N;
-    float L = N * parameters.space_step;
     int NDIMS = parameters.NDIMS;
-    int length = get_length();
-    int n_bins = N / 2 - 1; // N/2 - 1 because we ignore the k = 0 mode.
+    float L = N * parameters.space_step;
 
+    int length = get_length();
+
+    int n_bins = N / 2 - 1; // N/2 - 1 because we ignore the k = 0 mode.
     data_t *pk = (data_t *) calloc(n_bins, sizeof(data_t));
     data_t *ks = (data_t *) calloc(n_bins, sizeof(data_t));
     int *count = (int *) calloc(n_bins, sizeof(int)); // count the number of modes in each bin.
@@ -49,31 +50,30 @@ void output_powerspec(char *file_name, data_t *data_real, data_t *data_imag) {
 
     fftw_plan_with_nthreads(omp_get_max_threads());
 
-    fftw_complex *data, *data_k;
-    fftw_plan plan;
-
-    data   = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * length);
-    data_k = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * length);
-
-    #pragma omp parallel for schedule(static)
-    for (int m = 0; m < length; m++) {
-        data[m][0] = data_real[m];
-        data[m][1] = (data_imag != NULL) ? data_imag[m] : 0.0f;
-    }
-
     if (NDIMS == 2) {
-
-        shift2D(data, N);
-
-        plan = fftw_plan_dft_2d(N, N, data, data_k, FFTW_FORWARD, FFTW_ESTIMATE);
-        fftw_execute(plan);
 
         data_t *kx = (data_t *) calloc(N, sizeof(data_t));
         data_t *ky = (data_t *) calloc(N, sizeof(data_t));
-
         assert(kx != NULL && ky != NULL);
 
         for (int i = 0; i < N; i++) kx[i] = ky[i] = (- N / 2.0f + i);
+
+        fftw_complex *data, *data_k;
+        fftw_plan plan;
+
+        data   = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * length);
+        data_k = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * length);
+        plan = fftw_plan_dft_2d(N, N, data, data_k, FFTW_FORWARD, FFTW_ESTIMATE);
+
+        #pragma omp parallel for schedule(static)
+        for (int m = 0; m < length; m++) {
+            data[m][0] = data_real[m];
+            data[m][1] = (data_imag != NULL) ? data_imag[m] : 0.0f;
+        }
+
+        shift2D(data, N);
+
+        fftw_execute(plan);
 
         #pragma omp parallel for schedule(static)
         for (int m = 0; m < length; m++) {
@@ -89,30 +89,38 @@ void output_powerspec(char *file_name, data_t *data_real, data_t *data_imag) {
             count[ps_index]++;
         }
 
-        free(kx);
-        free(ky);
-
         for (int m = 0; m < n_bins; m++) {
             ks[m] /= count[m]; // compute average k value within bin.
             pk[m] /= 2.0f * M_PI * ks[m]; // angular average over '2-dimensional shell'
             ks[m] *= 2.0f * M_PI / L; // convert to dimensionful k.
         }
+
+        fftw_destroy_plan(plan);
+        fftw_free(data);
+        fftw_free(data_k);
+        free(kx);
+        free(ky);
     }
 
     if (NDIMS == 3) {
 
-        shift3D(data, N);
-
-        plan = fftw_plan_dft_3d(N, N, N, data, data_k, FFTW_FORWARD, FFTW_ESTIMATE);
-        fftw_execute(plan);
-
         data_t *kx = (data_t *) calloc(N, sizeof(data_t));
         data_t *ky = (data_t *) calloc(N, sizeof(data_t));
         data_t *kz = (data_t *) calloc(N, sizeof(data_t));
-
         assert(kx != NULL && ky != NULL && kz != NULL);
 
         for (int i = 0; i < N; i++) kx[i] = ky[i] = kz[i] = (- N / 2.0f + i);
+
+        fftw_complex *data, *data_k;
+        fftw_plan plan;
+
+        data   = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * length);
+        data_k = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * length);
+        plan = fftw_plan_dft_3d(N, N, N, data, data_k, FFTW_FORWARD, FFTW_ESTIMATE);
+
+        shift3D(data, N);
+
+        fftw_execute(plan);
 
         #pragma omp parallel for schedule(static)
         for (int m = 0; m < length; m++) {
@@ -128,22 +136,22 @@ void output_powerspec(char *file_name, data_t *data_real, data_t *data_imag) {
             count[ps_index]++;
         }
 
-        free(kx);
-        free(ky);
-        free(kz);
-
         for (int m = 0; m < n_bins; m++) {
             ks[m] /= count[m]; // compute average k value within bin.
             pk[m] /= 4.0f * M_PI * pow_2(ks[m]); // angular average over '3-dimensional shell'
             ks[m] *= 2.0f * M_PI / L; // convert to dimensionful k.
         }
+
+        fftw_destroy_plan(plan);
+        fftw_free(data);
+        fftw_free(data_k);
+        free(kx);
+        free(ky);
+        free(kz);
     }
 
     fio_save_pk(file_name, pk, ks, count, n_bins);
 
-    fftw_destroy_plan(plan);
-    fftw_free(data);
-    fftw_free(data_k);
     free(pk);
     free(ks);
     free(count);
